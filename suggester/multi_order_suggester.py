202fc535aeb1e865e9680cf4a559a18987aa942f
nonya <nonya579@gmail.com>
2022-09-19 00:12:52 +0700

    implement random assignment

--- suggester/multi_order_suggester.py
+++ suggester/multi_order_suggester.py
@@ -1,7 +1,16 @@
+import random
+
 class MultiOrderSuggester:
     def __init__(self):
         self.rider_simulator = None
         self.order_simulator = None
 
+    # randomly assign each order to a rider
     def assign_order_to_rider(self):
-        pass
+        rider_list = self.rider_simulator.rider_list #list of available Riders
+        order_list = self.order_simulator.order_list #list of Orders
+
+        for order in order_list:
+            rider = random.choice(rider_list)
+            self.rider_simulator.assign_order_to_a_rider(order, rider)
+        
