570adb416704aa4b8549600c801926a05edd6ce5
Watwe <anon.ongsakul2000@gmail.com>
2022-10-10 10:14:47 +0700

    change type_enum to common

--- common/location.py
+++ common/location.py
@@ -10,25 +10,33 @@ class LocationEnum(Enum):
 
 class Coordinates():
 
-    def __init__(self, x : float = 0, y : float = 0):
-        self.x = x
-        self.y = y
+    def __init__(self, *args, **kwargs):
+        if len(args) == 2:
+            self.x = args[0]
+            self.y = args[1]
 
-    def __init__(self, other):
-        self.x = other.x
-        self.y = other.y
+        elif len(args) == 1:
+            location = args[0]
+            self.x = location.x
+            self.y = location.y
+        elif len(args) == 0:
+            self.x = 0
+            self.y = 0
 
-    def __radd__(self, other):
+    def __add__(self, other):
         self.x += other.x
         self.y += other.y
+        return self
 
-    def __rsub__(self, other):
+    def __sub__(self, other):
         self.x -= other.x
         self.y -= other.y
+        return self
 
-    def __rdiv__(self, other):
+    def __truediv__(self, other):
         self.x /= other
         self.y /= other
+        return self
 
 
 def generateBangkokLocation_2() -> Coordinates:
