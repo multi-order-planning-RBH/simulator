652944d941928667e2c054593dae0a9c9746dc11
Watwe <anon.ongsakul2000@gmail.com>
2022-10-10 11:04:16 +0700

    add location estimation test

--- rider/test/non_zero_starting_test.py
+++ rider/test/non_zero_starting_test.py
@@ -22,27 +22,22 @@ order = Order(destination_location, resraurant_location, 1, created_time, ready_
 
 assert_statement = rider.current_action.action == ActionEnum.UNAVAILABLE
 assert assert_statement, "Should be the rider holding UNAVAILABLE status"
-print("[/] Pass - Holding UNAVAILABLE status.")
 
 rider.simulate(10)
 assert_statement = rider.current_action.action == ActionEnum.UNAVAILABLE
 assert assert_statement, "Should be the rider holding UNAVAILABLE status after passing 10 mins"
-print("[/] Pass - Holding UNAVAILABLE status at 10 mins.")
 
 rider.simulate(50)
 assert_statement = rider.current_action.action == ActionEnum.UNAVAILABLE
 assert assert_statement, "Should be the rider holding UNAVAILABLE status after passing 50 mins"
-print("[/] Pass - Holding UNAVAILABLE status at 50 mins.")
 
 rider.simulate(99)
 assert_statement = rider.current_action.action == ActionEnum.UNAVAILABLE
 assert assert_statement, "Should be the rider holding UNAVAILABLE status after passing 99 mins"
-print("[/] Pass - Holding UNAVAILABLE status at 99 mins.")
 
 rider.simulate(100)
 assert_statement = rider.current_action.action == ActionEnum.NO_ACTION
 assert assert_statement, "Should be the rider holding NO_ACTION status after passing 100 mins"
-print("[/] Pass - Holding NO_ACTION status at 100 mins.")
 
 rider.add_order_destination(order, 110)
 rider.simulate(110)
@@ -50,7 +45,6 @@ assert_statement = rider.current_action.action == ActionEnum.NO_ACTION and \
                     rider.next_action.action == ActionEnum.RIDING and \
                     len(rider.destinations) == 2
 assert assert_statement, "Should be the rider holding NO_ACTION status and being prepared for next order at 110 mins"
-print("[/] Pass - Holding NO_ACTION status at 110 mins.")
 
 rider.simulate(111)
 assert_statement = rider.current_action.action == ActionEnum.RIDING and \
@@ -61,7 +55,6 @@ assert_statement = rider.current_action.action == ActionEnum.RIDING and \
                     rider.destinations[0].location == resraurant_location
 
 assert assert_statement, "Should be the rider holding RIDING status at 111 mins"
-print("[/] Pass - Holding RIDING status at 111 mins.")
 
 rider.simulate(141)
 assert_statement = rider.current_action.action == ActionEnum.WAITING and \
@@ -71,12 +64,10 @@ assert_statement = rider.current_action.action == ActionEnum.WAITING and \
                     rider.next_action.time == 151 and \
                     rider.destinations[0].location == resraurant_location
 assert assert_statement, "The rider should be holding Waiting status at 141 mins"
-print("[/] Pass - Holding Waiting status at 141 mins.")
 
 rider.simulate(150)
 assert_statement = rider.current_action.action == ActionEnum.WAITING 
 assert assert_statement, "Should be the rider holding WAITING status after passing 150 mins"
-print("[/] Pass - Holding WAITING status at 150 mins.")
 
 rider.simulate(151)
 assert_statement = rider.current_action.action == ActionEnum.PICKUP_OR_DELIVER and \
@@ -86,7 +77,6 @@ assert_statement = rider.current_action.action == ActionEnum.PICKUP_OR_DELIVER a
                     rider.next_action.time == 152 and \
                     rider.destinations[0].location == destination_location
 assert assert_statement, "The rider should be holding PICKUP status at 151 mins"
-print("[/] Pass - Holding PICKUP status at 151 mins.")
 
 rider.simulate(152)
 assert_statement = rider.current_action.action == ActionEnum.RIDING and \
@@ -96,7 +86,9 @@ assert_statement = rider.current_action.action == ActionEnum.RIDING and \
                     rider.next_action.time == 182 and \
                     rider.destinations[0].location == destination_location
 assert assert_statement, "The rider should be holding RIDING status at 152 mins"
-print("[/] Pass - Holding RIDING status at 152 mins.")
+
+for time in range(153,182):
+    rider.simulate(time)
 
 rider.simulate(182)
 assert_statement = rider.current_action.action == ActionEnum.WAITING and \
@@ -104,9 +96,10 @@ assert_statement = rider.current_action.action == ActionEnum.WAITING and \
                     rider.current_action.time == 182 and \
                     rider.next_action.action == ActionEnum.PICKUP_OR_DELIVER and \
                     rider.next_action.time == 188 and \
-                    rider.destinations[0].location == destination_location
+                    rider.destinations[0].location == destination_location and \
+                    float(rider.location-rider.destinations[0].location) < 0.0001
 assert assert_statement, "The rider should be holding WAITING status at 182 mins"
-print("[/] Pass - Holding WAITING status at 182 mins.")
+print("[/] Pass - Location estimation.")
 
 rider.resting_prob = 1
 
@@ -117,13 +110,11 @@ assert_statement = rider.current_action.action == ActionEnum.PICKUP_OR_DELIVER a
                     rider.next_action.action == ActionEnum.NO_ACTION and \
                     rider.next_action.time == 189 
 assert assert_statement, "The rider should be holding PICKUP_OR_DELIVER status at 188 mins"
-print("[/] Pass - Holding PICKUP_OR_DELIVER status at 188 mins.")
 
 rider.simulate(189)
 assert_statement = rider.current_action.action == ActionEnum.NO_ACTION and \
                     len(rider.destinations) == 0  
 assert assert_statement, "The rider should be holding NO_ACTION status at 189 mins"
-print("[/] Pass - Will hold NO_ACTION status at 189 mins.")
 
 rider.simulate(190)
 assert_statement = rider.current_action.action == ActionEnum.NO_ACTION and \
@@ -132,7 +123,6 @@ assert_statement = rider.current_action.action == ActionEnum.NO_ACTION and \
                     rider.next_action.time == 191
                     
 assert assert_statement, "The rider should be holding NO_ACTION status at 190 mins"
-print("[/] Pass - Will hold NO_ACTION status at 190 mins.")
 
 rider.simulate(191)
 assert_statement = rider.current_action.action == ActionEnum.RESTING and \
@@ -142,7 +132,6 @@ assert_statement = rider.current_action.action == ActionEnum.RESTING and \
                     rider.next_action.time == 221
                     
 assert assert_statement, "The rider should be holding RESTING status at 191 mins"
-print("[/] Pass - Will hold RESTING status at 191 mins.")
 
 rider.resting_prob = 0.01
 
@@ -152,14 +141,16 @@ assert_statement = rider.current_action.action == ActionEnum.NO_ACTION and \
                     len(rider.destinations) == 0 
                     
 assert assert_statement, "The rider should be holding NO_ACTION status at 221 mins"
-print("[/] Pass - Will hold NO_ACTION status at 221 mins.")
 
 rider.simulate(480)
 assert_statement = rider.current_action.action == ActionEnum.UNAVAILABLE
 assert assert_statement, "Should be the rider holding UNAVAILABLE status after passing 480 mins"
-print("[/] Pass - Holding UNAVAILABLE status at 480 mins.")
 
 rider.simulate(481)
 assert_statement = rider.current_action.action == ActionEnum.UNAVAILABLE
 assert assert_statement, "Should be the rider holding UNAVAILABLE status after passing 480 mins"
-print("[/] Pass - Holding UNAVAILABLE status at 480 mins.")
+
+print("[/] Pass - non zero starting time initializing.")
+print("[/] Pass - rider assigning.")
+print("[/] Pass - rider resting.")
+print("[/] Pass - rider getting off.")
