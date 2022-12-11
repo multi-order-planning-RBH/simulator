996979c214a39ec69dc04dc2cf2a07deda9f9368
nonya <nonya579@gmail.com>
2022-12-11 21:58:23 +0700

    add order graph construction

--- suggester/multi_order_suggester.py
+++ suggester/multi_order_suggester.py
@@ -1,7 +1,8 @@
 import random
 from common.action import ActionEnum
-from order_restaurant.order_restaurant_simulator import OrderSimulator
+from order_restaurant.order_restaurant_simulator import Order, OrderSimulator
 from rider.rider_simulator import RiderSimulator
+from suggester.types.batch import Batch
 
 class MultiOrderSuggester:
     def __init__(self, rider_simulator: RiderSimulator, order_simulator: OrderSimulator):
@@ -23,6 +24,31 @@ class MultiOrderSuggester:
                 rider = random.choice(rider_list)
                 self.rider_simulator.assign_order_to_a_rider(order, rider, time)
                 rider_list.remove(rider)"""
+    
+    def construct_order_graph(self, orders: list[Order]) -> dict[Batch, dict[Batch, int]]:
+        batches = list()
+        for order in orders:
+            batch = Batch([order], [order.restaurant_location, order.destination])
+            batches.append(batch)
+
+        order_graph = dict()
+        for batch in batches:
+            edges = dict()
+            for neighbor in batches:
+                if neighbor == batch:
+                    continue
+                
+                edges[neighbor] = self.calculate_order_graph_weight(batch, neighbor)
+
+            order_graph[batch] = edges
+        
+        return order_graph
+    
+    def calculate_order_graph_weight(self, batch: Batch, neighbor: Batch) -> int:
+        return 0
+
+        
+
                 
         
         
