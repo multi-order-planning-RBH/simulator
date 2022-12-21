e13f22ff2e2102f5b35af0167c741002bb6b75bd
Chawak <mondcha1507@gmail.com>
2022-12-22 00:06:41 +0700

    (fix) fix destination field in Order field to Destination class

--- order_restaurant/order_restaurant_simulator.py
+++ order_restaurant/order_restaurant_simulator.py
@@ -5,19 +5,20 @@ sys.path.append(os.path.abspath("..\\simulator"))
 import numpy as np
 import pandas as pd
 import random
-from common.location import generateBangkokLocation ,Coordinates
+from common.location import generateBangkokLocation, Coordinates
 from common.order import OrderEnum
 import scipy.stats
+from common.location import Coordinates, LocationEnum, generateBangkokLocation
 
 class Restaurant:
 
     def __init__(self,location,restaurant_idx,mean=1000,std=300):
-        self.id = restaurant_idx
-        self.location = location
+        self.id : int = restaurant_idx
+        self.location : Coordinates = location
         # queue of orderId
-        self.order_id_queue = []
-        self.mean=mean
-        self.std=std
+        self.order_id_queue : list[int]= []
+        self.mean : int =mean
+        self.std : int =std
  
     def rider_pickup_order(self,pickup_order):
         # check if pickup_order ready if yes
@@ -55,9 +56,9 @@ class Restaurant:
 
 class RestaurantSimulator :
     def __init__(self):
-        self.restaurant_idx=0
-        self.restaurant_list = []
-        self.restaurant_id_list = []
+        self.restaurant_idx : int =0
+        self.restaurant_list : list[Restaurant] = []
+        self.restaurant_id_list : list[int]= []
         # res_list = pd.read_csv("order_restaurant/restaurant_sample.csv")
         res_list = pd.read_csv("order_restaurant/restaurant_sample_10000.csv")
         for idx,res in res_list.iterrows():
@@ -101,19 +102,19 @@ class RestaurantSimulator :
 
 class Order:
 
-    def __init__(self, destination,restaurant_id,restaurant_location,order_idx,created_time):
-        self.id = order_idx
-        self.restaurant = restaurant_id
-        self.restaurant_location = restaurant_location
-        self.destination = destination
-        self.created_time = created_time
-        self.assigned_time = None
-        self.meal_finished_time = None
-        self.picked_up_time = None
-        self.cooking_duration = None
-        self.finished_time = 0
-        self.status = OrderEnum.CREATED
-        self.rider=None
+    def __init__(self,restaurant_id,order_idx,created_time):
+        self.id : int = order_idx
+        self.restaurant : int = restaurant_id
+        self.restaurant_destination : Destination = None
+        self.customer_destination : Destination = None
+        self.created_time : int = created_time
+        self.assigned_time : int = None
+        self.meal_finished_time : int = None
+        self.picked_up_time : int = None
+        self.cooking_duration : int = None
+        self.finished_time : int = 0
+        self.status : OrderEnum = OrderEnum.CREATED
+        self.rider =None
 
         
 class OrderSimulator:
@@ -137,17 +138,18 @@ class OrderSimulator:
         self.unassigned_order_list = [o for o in self.unassigned_order_list if o.id not in cancelled_id]
 
         restaurant_id=random.choice(restaurant_simulator.get_all_restaurant_id())
-        destination = generateBangkokLocation()
-        self.create_order(destination,restaurant_id,time)
+        customer_destination = generateBangkokLocation()
+        self.create_order(customer_destination,restaurant_id,time)
 
-    def create_order(self,destination,restaurant_id,created_time):
+    def create_order(self,customer_destination,restaurant_id,created_time):
         
         restaurant = restaurant_simulator.get_restaurant_by_id(restaurant_id)
-
-        new_order=Order(destination, restaurant_id, restaurant.location, self.order_idx, created_time)
-        
+        new_order=Order(restaurant_id, self.order_idx, created_time)
         new_order.cooking_duration= restaurant_simulator.estimate_real_cooking_duration(restaurant_id,new_order)
         
+        new_order.restaurant_destination = Destination(new_order, restaurant.location, LocationEnum.RESTAURANT, new_order.cooking_duration)
+        new_order.customer_destination = Destination(new_order, customer_destination, LocationEnum.CUSTOMER, 5)
+
         self.order_dict[self.order_idx]=new_order
 
         self.unassigned_order_list.append(new_order)
@@ -196,6 +198,19 @@ class OrderSimulator:
         except:
             print("Order with Id",order_id,"is not found.")
 
-
+class Destination :
+    def __init__(self, order : Order, location : Coordinates, type : LocationEnum, ready_time : int):
+        self.location : Coordinates = location
+        self.type : LocationEnum = type
+        self.ready_time : int = ready_time
+        self.order : Order = order
+    
+    def pick_up_or_deliver(self, time):
+        if self.type == LocationEnum.RESTAURANT:
+            self.order.status = OrderEnum.PICKED_UP
+            order_simulator.change_order_status(self.order.id,OrderEnum.PICKED_UP, time)
+        elif self.type == LocationEnum.CUSTOMER:
+            self.order.status = OrderEnum.DELIVERED
+            order_simulator.change_order_status(self.order.id,OrderEnum.DELIVERED, time)
 order_simulator = OrderSimulator()
 restaurant_simulator = RestaurantSimulator()
