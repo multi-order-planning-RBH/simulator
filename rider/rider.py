5008358258101c48efeac4ab7199913f5007053e
Watwe <anon.ongsakul2000@gmail.com>
2023-01-22 11:32:33 +0700

    edit rider state and apply path geometry

--- rider/rider.py
+++ rider/rider.py
@@ -1,15 +1,21 @@
-from argparse import Action
-from typing import Dict, List
 import random
 import sys, os
+
+from argparse import Action
+from typing import Dict, List
+from math import ceil
+from shapely import Point
+
 sys.path.append(os.path.abspath("./"))
-from common.location import Coordinates, LocationEnum, generateBangkokLocation
+
+from common.location import LocationEnum
 from common.action import ActionEnum
 from common.status import StatusEnum
-#from order.order_simulator import Order
 from rider.estimator import getEstimatedTimeTraveling
 from order_restaurant.order_restaurant_simulator import Order, Destination
 from suggester.types.batch import Batch
+from map.map import get_geometry_of_path
+from config import Config
 
 
 class Action : 
@@ -18,9 +24,9 @@ class Action :
         self.time : int = time 
 
 class Rider:
-    def __init__(self, id:int, starting_time:int = 0, getoff_time:int = 480, resting_time:int = 30):
+    def __init__(self, id:int, location:Point, starting_time:int = 0, getoff_time:int = 480, resting_time:int = 30):
         self.id : int = id
-        self.location : Coordinates = generateBangkokLocation()
+        self.location : Point = location
         self.starting_time : int = starting_time
         self.getoff_time : int = getoff_time
         self.resting_time : int = resting_time
@@ -28,17 +34,22 @@ class Rider:
         self.destinations : List[Destination] = list()
         self.next_action : Action = None 
         self.status : StatusEnum = StatusEnum.WORKING
-        if starting_time == 0:
-            self.current_action : Action = Action(ActionEnum.NO_ACTION, 0)
-        else :
-            self.current_action : Action = Action(ActionEnum.UNAVAILABLE, 0)
-            self.next_action : Action = Action(ActionEnum.NO_ACTION, starting_time)
+        self.current_action : ActionEnum = ActionEnum.NO_ACTION
+        self.done_current_action_time : int = 0
+        # if starting_time == 0:
+        #     self.current_action : Action = Action(ActionEnum.NO_ACTION, 0)
+        # else :
+        #     self.current_action : Action = Action(ActionEnum.UNAVAILABLE, 0)
+        #     self.next_action : Action = Action(ActionEnum.NO_ACTION, starting_time)
         self.log : Dict[int, list] = dict()
-        self.speed : Coordinates = Coordinates()
-        self.utilization_time : int = 0
+        self.speed : float = Config.RIDER_SPEED
         self.working_time : int = self.getoff_time - self.starting_time
         self.current_destination : Destination = None
         self.order_count : int = 0
+
+        self.path = None
+        self.time_traveling = 0
+        self.t = 0
 
     def add_order_destination(self, order : Order, time : int) -> bool:
         if (self.current_action != ActionEnum.RESTING or \
@@ -73,86 +84,62 @@ class Rider:
         self.speed = (destination - self.location)/traveling_time
 
     def logging(self, time):
-        action = self.current_action.time
-        if len(self.destinations) > 0:
-            destination_x = self.destinations[0].location.x
-            destination_y = self.destinations[0].location.y
-        else :
-            destination_x = destination_y = None
-        temp = [time, action, destination_x, destination_y]
+        action = self.current_action
+        location_x = self.location.x
+        location_y = self.location.x
+        temp = [time, action, location_x, location_y]
         self.log[time] = temp
 
     def simulate(self, time : int) -> ActionEnum:
-        self.location += self.speed
-        if self.next_action == None:
-            if self.current_action.action == ActionEnum.NO_ACTION and len(self.destinations) > 0: 
-                next_action = ActionEnum.RIDING
-                next_time = time + 1
-                self.next_action = Action(next_action, next_time)
-            elif random.uniform(0, 1)<self.resting_prob: 
-                next_action = ActionEnum.RESTING
-                next_time = time + 1
-                self.next_action = Action(next_action, next_time)
-
-        elif time >= self.next_action.time : 
-            self.current_action = self.next_action
+        if time % Config.RIDER_LOGGING_PERIOD == 0:
             self.logging(time)
 
-            if self.current_action.action == ActionEnum.RIDING:
-                next_action = ActionEnum.WAITING
-                next_time = getEstimatedTimeTraveling()
-                self.calculate_speed(next_time)
-                self.current_destination = self.destinations.pop(0)
-                self.utilization_time += next_time
-                next_time += time
-                self.next_action = Action(next_action, next_time)
+        if self.current_action == ActionEnum.NO_ACTION:
+            if len(self.destinations) > 0:
+                self.current_action = ActionEnum.RIDING
+                # Need to specify
+                origin = self.location
+                dest = self.destinations[0].location
+                self.path = get_geometry_of_path(origin, dest)
+                self.time_traveling = ceil(self.path.length/self.speed)
+                self.t = 1
 
-            elif self.current_action.action == ActionEnum.WAITING:
-                self.speed = Coordinates()
-                next_action = ActionEnum.PICKUP_OR_DELIVER
+            elif random.uniform(0, 1)<self.resting_prob: 
+                self.current_action = ActionEnum.RESTING
+                self.done_current_action_time = time + self.resting_time
+
+        elif self.current_action == ActionEnum.RIDING:
+            self.location = self.path.interpolate(self.speed*self.t)
+            self.t += 1
+            if self.t > self.time_traveling:
+                self.current_action = ActionEnum.WAITING
                 destination = self.current_destination
                 order = destination.order
+                if order and destination.type == LocationEnum.RESTAURANT:
+                    self.done_current_action_time = order.meal_finished_time
+                else : 
+                    self.done_current_action_time = time
 
-                #Add additional time for waiting customer to come for pick up the order when riding to the customer
-                meal_finished_time = order.meal_finished_time if destination.type == LocationEnum.RESTAURANT else time
+        elif self.current_action == ActionEnum.WAITING:
+            if time > self.done_current_action_time:
+                self.current_action = ActionEnum.ACTION
 
-                #Compare waiting time and commuting time
-                next_time = max(meal_finished_time, self.current_action.time)+1
+        elif self.current_action == ActionEnum.ACTION:
+            self.current_destination.action(time)
+            self.current_action = ActionEnum.NO_ACTION
 
-                self.next_action = Action(next_action, next_time)
+        elif self.current_action == ActionEnum.RESTING:
+            if time > self.done_current_action_time:
+                self.current_action = ActionEnum.ACTION
 
-            elif self.current_action.action == ActionEnum.PICKUP_OR_DELIVER:
-                self.speed = Coordinates()
-                self.current_destination.pick_up_or_deliver(time)
-                if len(self.destinations) == 0:
-                    next_action = ActionEnum.NO_ACTION
-                else:
-                    next_action = ActionEnum.RIDING
-                next_time = time + 1
-                self.next_action = Action(next_action, next_time)
+        else:
+            pass
 
-            
-            elif self.current_action.action == ActionEnum.RESTING :
-                self.speed = Coordinates()
-                self.current_destination = None
-                next_action = ActionEnum.NO_ACTION
-                next_time = time + self.resting_time
-                self.next_action = Action(next_action, next_time)
-            
-            elif self.current_action.action == ActionEnum.NO_ACTION :
-                self.speed = Coordinates()
-                self.current_destination = None
-                self.next_action = None
-
-        #Need to concern about remaining order
         if time >= self.getoff_time:
             self.current_destination = None
-            next_action = ActionEnum.UNAVAILABLE
-            next_time = time+1
-            self.current_action = Action(next_action, next_time)
-            self.next_action = None
+            self.current_action = ActionEnum.UNAVAILABLE 
             
-        return self.current_action.action
+        return self.current_action
 
         
 
