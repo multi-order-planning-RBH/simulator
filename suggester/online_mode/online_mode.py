617bbc2589b754ed83b9eb06fc9eb7caeb459b17
nonya123456 <nonya579@gmail.com>
2023-01-23 08:45:52 +0700

    implement max order

--- suggester/online_mode/online_mode.py
+++ suggester/online_mode/online_mode.py
@@ -1,22 +1,32 @@
+from ml_estimator.traveling_time import estimate_traveling_time
+from common.location import LocationEnum
+from rider.rider import Destination
+from rider.rider import Rider
+from order_restaurant.order_restaurant_simulator import Order
 import sys
 import os
 import numpy as np
 
 sys.path.append(os.path.abspath("./"))
-from order_restaurant.order_restaurant_simulator import Order
-from rider.rider import Rider
-from rider.rider import Destination
-from common.location import LocationEnum
-from ml_estimator.traveling_time import estimate_traveling_time
+
 
 class OnlineMode:
-    def __init__(self):
-        # TODO: add max order
-        pass
+    def __init__(self, max_order=3):
+        self.max_order = max_order
 
     def find_best_insertion(self, order: Order, riders: list[Rider], time: int):
         min_cost = np.inf
         for rider in riders:
+            num_orders = 0
+            if rider.current_destination is not None and rider.current_destination.type == LocationEnum.CUSTOMER:
+                num_orders += 1
+            for destination in rider.destinations:
+                if destination.type == LocationEnum.CUSTOMER:
+                    num_orders += 1
+
+            if num_orders >= self.max_order:
+                continue
+
             cost, destinations = self.plain_insertion(order, rider, time)
             if cost < min_cost:
                 min_cost = cost
@@ -32,11 +42,13 @@ class OnlineMode:
             new_destinations.append(order.restaurant_destination)
             new_destinations.append(order.customer_destination)
 
-            new_finished_time = self.calculate_finished_time(new_destinations, order.restaurant_destination, time)
+            new_finished_time = self.calculate_finished_time(
+                new_destinations, order.restaurant_destination, time)
             cost = new_finished_time - time
             return cost, new_destinations
 
-        old_finished_time = self.calculate_finished_time(rider.destinations, rider.current_destination, time)
+        old_finished_time = self.calculate_finished_time(
+            rider.destinations, rider.current_destination, time)
         min_cost = np.inf
         for i in range(len(rider.destinations)):
             for j in range(i + 1, len(rider.destinations) + 2):
@@ -44,7 +56,8 @@ class OnlineMode:
                 new_destinations.insert(i, order.restaurant_destination)
                 new_destinations.insert(j, order.customer_destination)
 
-                new_finished_time = self.calculate_finished_time(new_destinations, rider.current_destination, time)
+                new_finished_time = self.calculate_finished_time(
+                    new_destinations, rider.current_destination, time)
                 cost = new_finished_time - old_finished_time
                 if min_cost > cost:
                     min_cost = cost
@@ -59,18 +72,23 @@ class OnlineMode:
         current_time = time
         for idx in range(len(destinations)):
             if idx == 0:
-                current_time += estimate_traveling_time(current_destination.location, destinations[idx].location)
+                current_time += estimate_traveling_time(
+                    current_destination.location, destinations[idx].location)
             else:
-                current_time += estimate_traveling_time(destinations[idx - 1].location, destinations[idx].location)
+                current_time += estimate_traveling_time(
+                    destinations[idx - 1].location, destinations[idx].location)
 
             if destinations[idx].type == LocationEnum.RESTAURANT:
                 if destinations[idx].order.assigned_time is None:
                     # new order has not been assigned. so we use time + estimated preparing time
-                    current_time = max(current_time, time + destinations[idx].preparing_duration)
+                    current_time = max(current_time, time +
+                                       destinations[idx].preparing_duration)
                 else:
                     # assigned order
-                    current_time = max(current_time, destinations[idx].order.assigned_time + destinations[idx].preparing_duration)
-                
+                    current_time = max(
+                        current_time, destinations[idx].order.assigned_time + destinations[idx].preparing_duration)
+
         return current_time
+
 
 online_mode = OnlineMode()
